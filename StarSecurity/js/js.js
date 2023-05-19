b2e418a86bee1367fa185ed2bfdcf94aeddcb07e
TranTheSon2206 <109707502+TranTheSon2206@users.noreply.github.com>
2023-05-20 00:02:45 +0700

    Add files via upload

---
+++ StarSecurity/js/js.js
@@ -0,0 +1,47 @@
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+/* Menu con của services */
+const servicesToggle = document.getElementById("services-toggle");
+const servicesDropdown = document.getElementById("services-dropdown");
+
+servicesToggle.addEventListener("click", function() {
+  servicesDropdown.classList.toggle("show");
+});
+
+document.addEventListener("click", function(e) {
+  if (!servicesToggle.contains(e.target) && !servicesDropdown.contains(e.target)) {
+    servicesDropdown.classList.remove("show");
+  }
+});
+
